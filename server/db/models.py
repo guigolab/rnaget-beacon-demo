b10b9f462ae3e9d444d6f9b3c1c13fab7c6551ad
Emilio Righi <righiemilio@gmail.com>
2024-07-31 10:38:47 +0200

    wip

--- server/db/models.py
+++ server/db/models.py
@@ -1,28 +1,52 @@
 from . import db, enums
 
+class OntologyTerm(db.EmbeddedDocument):
+    id = db.StringField() #CURIE
+    label = db.StringField()
+
 class BioSample(db.Document):
-    biosample_id = db.StringField(required=True,unique=True)
+    id = db.StringField(primary_key=True)
     matrices = db.ListField(db.StringField())
+    meta = {
+        'indexes': ['id']
+    }
 
 class ExpressionValue(db.Document):
-    matrix_id = db.StringField(required=True,unique=True)
-    sequence_id = db.StringField(required=True)
-    biosample_id = db.StringField(required=True)
+    sequenceID = db.StringField(required=True)
+    biosampleID = db.StringField(required=True)
+    matrixID = db.StringField(required=True)
     value = db.FloatField(required=True)
+    meta = {
+        'indexes': [
+            {
+                'fields': ['sequenceID', 'biosampleID','matrixID'],
+                'unique': True  # This enforces uniqueness
+            }
+        ],
+        'strict': False
+    }
 
 class SequenceFeature(db.Document):
-    sequence_id = db.StringField(required=True, unique= True)
-    biological_functions=db.ListField(db.StringField())
-    molecular_type=db.StringField()
+    id = db.StringField(primary_key=True)
+    biologicalFunctions=db.ListField(db.EmbeddedDocumentField(OntologyTerm))
+    molecularType=db.EmbeddedDocumentField(OntologyTerm)
     matrices=db.ListField(db.StringField())
+    meta = {
+        'indexes': ['id','molecularType']
+    }
 
 class ExpressionMatrix(db.Document):
-    matrix_id = db.StringField(required=True, unique=True)
+    id = db.StringField(primary_key=True)
     name=db.StringField(required=True)
     description=db.StringField()
-    tax_id=db.StringField()
-    download_link=db.URLField()
+    taxID=db.StringField()
+    downloadLink=db.URLField()
     unit = db.EnumField(enums.Units)
-    reference_annotation = db.StringField(required=True)
-    rows=db.IntField()
-    columns=db.IntField()
+    referenceAnnotation = db.StringField(required=True)
+    featuresCount=db.IntField()
+    biosamplesCount=db.IntField()
+    meta = {
+        'indexes': ['id','name','taxID']
+    }
+
+
